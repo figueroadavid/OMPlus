bd77c638d81db42cd81c7db831b1515a8bfe02f2
DavidFigueroa <david.figueroa@harrishealth.org>
2020-12-23 16:22:35 -0600

    Update .psm1 to use $PSScriptRoot

--- OMPlus.psm1
+++ OMPlus.psm1
@@ -1,9 +1,9 @@
-Get-ChildItem -Path .\Private -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
+Get-ChildItem -Path $PSScriptRoot\Private -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
     ForEach-Object {
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
         . $_.FullName }
 
-Get-ChildItem -Path .\Public -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
+Get-ChildItem -Path $PSScriptRoot\Public -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
     ForEach-Object {
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
         . $_.FullName
