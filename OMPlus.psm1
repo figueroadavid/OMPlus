37e2fcb67aea8e51b4ad3b3b3fe222de791ea5ad
DavidFigueroa <david.figueroa@harrishealth.org>
2020-12-29 08:37:25 -0600

    Removed the scriptblock for argumentcompletions

--- OMPlus.psm1
+++ OMPlus.psm1
@@ -1,32 +1,3 @@
-if ($PSVersionTable.PSEdition -ne 'Core')
-{
-  # add the attribute [ArgumentCompletions()]:
-  $code = @'
-using System;
-using System.Collections.Generic;
-using System.Management.Automation;
-
-    public class ArgumentCompletionsAttribute : ArgumentCompleterAttribute
-    {
-
-        private static ScriptBlock _createScriptBlock(params string[] completions)
-        {
-            string text = "\"" + string.Join("\",\"", completions) + "\"";
-            string code = "param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams);@(" + text + ") -like \"*$WordToComplete*\" | Foreach-Object { [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }";
-            return ScriptBlock.Create(code);
-        }
-
-        public ArgumentCompletionsAttribute(params string[] completions) : base(_createScriptBlock(completions))
-        {
-        }
-    }
-'@
-
-  $null = Add-Type -TypeDefinition $code *>&1
-  Remove-Variable -Name code
-  # This is from https://powershell.one/powershell-internals/attributes/auto-completion
-}
-
 Get-ChildItem -Path $PSScriptRoot\Private -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
     ForEach-Object {
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
