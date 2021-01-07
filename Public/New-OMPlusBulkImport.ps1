88cf541efa6aa9f9a90b677562c7c7286a65ee1f
DavidFigueroa <david.figueroa@harrishealth.org>
2021-01-07 10:51:04 -0600

    Minor correction

--- Public/New-OMPlusBulkImport.ps1
+++ Public/New-OMPlusBulkImport.ps1
@@ -36,22 +36,31 @@ function New-OMPlusBulkImport {
         [char]$Delimiter = ','
     )
 
-    $CSV = Import-CSV -Path $FilePath -Delimiter $Delimiter
-    $SuppliedProperties = $CSV | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+    Begin {
+        $CSV = Import-CSV -Path $FilePath -Delimiter $Delimiter
+        $SuppliedProperties = $CSV | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
 
-    foreach ($Record in $CSV) {
-        $OMSplat = @{}
-        foreach ($Property in $SuppliedProperties) {
-            if ($Record.$Property -match 'TRUE|FALSE') {
-                switch ($Record.$Property) {
-                    'TRUE'  {$OMSplat[$Property] = $TRUE; break }
-                    'FALSE' {$OMSplat[$Property] = $FALSE }
+        if ($CSV.Count -gt 10) {
+            $ShowProgress = $true
+        }
+
+    }
+
+    Process {
+        foreach ($Record in $CSV) {
+            $OMSplat = @{}
+            foreach ($Property in $SuppliedProperties) {
+                if ($Record.$Property -match 'TRUE|FALSE') {
+                    switch ($Record.$Property) {
+                        'TRUE'  {$OMSplat[$Property] = $TRUE; break }
+                        'FALSE' {$OMSplat[$Property] = $FALSE }
+                    }
+                }
+                else {
+                    $OMSplat[$Property] = $Record.$Property
                 }
             }
-            else {
-                $OMSplat[$Property] = $Record.$Property
-            }
+            New-OMPlusPrinter @OMSplat
         }
-        New-OMPlusPrinter @OMSplat
     }
 }
