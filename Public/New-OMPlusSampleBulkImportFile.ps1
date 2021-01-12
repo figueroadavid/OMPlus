5e4c7dbece30ca36ccb489e2443bd196489b0ddf
DavidFigueroa <david.figueroa@harrishealth.org>
2021-01-12 15:17:51 -0600

    Corrected the function to work properly

--- Public/New-OMPlusSampleBulkImportFile.ps1
+++ Public/New-OMPlusSampleBulkImportFile.ps1
@@ -81,32 +81,35 @@ function New-OMPlusSampleBulkImportFile {
     )
 
     $CommentsTable = @{
-        'PrinterName'           = 'Mandatory parameter; this is used to create the actual printer; spaces are not allowed'
-        'IPAddress'             = 'Mandatory parameter; the IP address for the printer, or LPR/LPD print server'
-        'PortType'              = 'Mandatory parameter; this indicates if the connection is TCP/IP or LPR/LPD, the correct header column will be output either TCPPort or LPRPort'
-        'Comment'               = 'Optional parameter; this is an comment for the printer'
-        'Notes'                 = 'Optional parameter; this is a a string for the Notes field'
-        'DoNotValidate'         = 'Optional parameter; this indicates the -z should be used so that lpadmin does not verify if the printer is actually up in order to create it'
-        'PurgeTime'             = 'Optional parameter; this is the amount of time in seconds before a print job is purged after changing status to prntd or can'
-        'PageLimit'             = 'Optional parameter; this is the maximum number of pages per job that can be printed out on this printer'
-        'SupportNotes'          = 'Optional parameter; this is an optional parameter for the Support field'
-        'WriteTimeout'          = 'Optional parameter; this indicates how long a print job can take to print before being cancelled in seconds'
-        'TranslationTable'      = 'Optional parameter; this is for an alternate translation table that needs to be used'
-        'DriverType'            = 'Optional parameter; this is supported DriverType for the printer'
+        'PrinterName'           = 'Mandatory parameter; Name used to create the actual printer; spaces are not allowed'
+        'IPAddress'             = 'Mandatory parameter; IP address for the printer, or LPR/LPD print server'
+        'PortType'              = 'Mandatory parameter; Flags the printer as TCP/IP or LPR/LPD, the correct header column will be output either TCPPort or LPRPort'
+        'TCPPort'               = 'Mandatory parameter: The TCP port used for network communication, between 0 and 65535; the default is 9100'
+        'LPRPort'               = 'Mandatory parameter: This is the name of the queue on the LPR/LPD server; the name is generally case-sensitive'
+        'Comment'               = 'Optional parameter; Comment for the printer'
+        'Notes'                 = 'Optional parameter; Notes field for the printer'
+        'DoNotValidate'         = 'Optional parameter; Tells lpadmin not to verify the printer before creating it (-z)'
+        'PurgeTime'             = 'Optional parameter; The amount of time in seconds before a print job is purged after changing status to prntd or can'
+        'PageLimit'             = 'Optional parameter; The maximum number of pages per job that can be printed out on this printer'
+        'SupportNotes'          = 'Optional parameter; Support field for the printer'
+        'WriteTimeout'          = 'Optional parameter; How long a print job can take to print before being cancelled (in seconds)'
+        'TranslationTable'      = 'Optional parameter; Alternate translation table that needs to be used'
+        'DriverType'            = 'Optional parameter; The DriverType for the printer; must be one of the supported ones from the system'
         'PCAPPath'              = 'Optional parameter; if this is included, it indicates the file location for a network pcap to be taken for the printer, and that the pcap should be taken'
-        'UserFilterPath'        = 'Optional parameter; this is used for a user supplied filter for the print jobs'
-        'Filter2'               = 'Optional parameter; this is used for a secondary user supplied filter for the print jobs'
-        'Filter3'               = 'Optional parameter; this is used for a tertiary user supplied filter for the print jobs'
-        'CPSMetering'           = 'Optional parameter; this provides for a characters per second limit for the printer'
-        'InsertMissingFF'       = 'Optional parameter; this inserts a form feed between jobs if it is not already present'
-        'FormType'              = 'Optional parameter; this sets the form type for the printer'
-        'LFtoCRLF'              = 'Optional parameter; this converts line feeds to Carriage Return/Line Feeds'
-        'CopyBreak'             = 'Optional parameter; this inserts a form feed between each copy of a multi-copy print job'
-        'FileBreak'             = 'Optional parameter; this inserts a form feed between print job files'
-        'Banner'                = 'Optional parameter; this inserts a print banner for each print job'
-        'HasInternalWebServer'  = 'Optional parameter; this indicates that the printer has a built in web server; if a CustomURL is not supplied it will attempt to create a URL from http://<ipaddress> or https://<ipaddress> '
-        'CustomURL'             = "Optional parameter; this indicates that the printer's web server has a custom URL that needs to be used"
-        'ForceWebServer'        = 'Optional parameter; this indicates that the default web server URL needs to be set even if neither http://<ipaddress> nor https://<ipaddress> respond '
+        'UserFilterPath'        = 'Optional parameter; User supplied filter for the print jobs'
+        'Filter2'               = 'Optional parameter; Secondary user supplied filter for the print jobs'
+        'Filter3'               = 'Optional parameter; Tertiary user supplied filter for the print jobs'
+        'CPSMetering'           = 'Optional parameter; Characters per second limit for the printer'
+        'InsertMissingFF'       = 'Optional parameter; Inserts a form feed between jobs if it is not already present'
+        'FormType'              = 'Optional parameter; Sets the form type for the printer'
+        'LFtoCRLF'              = 'Optional parameter; Converts line feeds to Carriage Return/Line Feeds'
+        'CopyBreak'             = 'Optional parameter; Inserts a form feed between each copy of a multi-copy print job'
+        'FileBreak'             = 'Optional parameter; Inserts a form feed between print job files'
+        'Banner'                = 'Optional parameter; Inserts a print banner for each print job'
+        'HasInternalWebServer'  = 'Optional parameter; Indicates that the printer has a built in web server; if a CustomURL is not supplied it will attempt to create a URL from http://<ipaddress> or https://<ipaddress> '
+        'CustomURL'             = "Optional parameter; Indicates that the printer's web server has a custom URL that needs to be used"
+        'ForceWebServer'        = 'Optional parameter; Indicates that te default web server URL needs to be set even if neither http://<ipaddress> nor https://<ipaddress> respond '
+        'IsTesting'             = 'Optional parameter; Causes the script to return the generated command line rather than execute it'
     }
     $ParameterSet = New-Object -Type System.Collections.Generic.List[string]
     $ParameterSet.Add('PrinterName')
@@ -119,15 +122,19 @@ function New-OMPlusSampleBulkImportFile {
             default { $ParameterSet.AddRange($OptionalParameter)}
         }
     }
+    $Tab = "`t"
+    $CRLF = "`r`n"
 
     if ($IncludeComments) {
-        $FileOutput = New-Object -Type System.Collections.Specialized.OrderedDictionary
-        foreach ($Parameter in $ParameterSet) {
-            $FileOutput.Add($Parameter, $CommentsTable.$Parameter)
+        $OutputHash = [ordered]@{}
+        $ParameterSet | ForEach-Object { 
+            $null = $OutputHash.Add($_, $CommentsTable.$_)
         }
-        $FileOutput | Export-CSV -Path $FilePath -Delimiter $Delimiter -NoTypeInformation
+        $OutputPSCO = [pscustomobject]$OutputHash
+        $OutputPSCO | Export-CSV -Path $FilePath -Delimiter $Delimiter -NoTypeInformation 
     }
     else {
-        $ParameterSet -join $Delimiter | Out-File -FilePath $FilePath
+        $ParameterSet -join ',' | Out-File -FilePath $FilePath
     }
+    
 }
