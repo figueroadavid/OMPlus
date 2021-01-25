84b9b89d3e77a338989acd71f4414c9dec0aae70
DavidFigueroa <david.figueroa@harrishealth.org>
2021-01-25 16:21:08 -0600

    Updated for multiple PS versions

--- Private/Get-OMPlusTypeTable.ps1
+++ Private/Get-OMPlusTypeTable.ps1
@@ -1,63 +1,123 @@
-function Get-OMPlusTypeTable {
-    [cmdletbinding()]
-    param(
-        [parameter(Mandatory)]
-        [ArgumentCompleter({
-            param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-                Get-OMPlusDriverNames | Select-object -ExpandProperty 'Driver' |
-                Where-Object { $_ -like "$WordToComplete*"} |
-                Sort-Object |
-                Foreach-Object {
-                    [System.Management.Automation.CompletionResult]::new(
-                        $_,
-                        $_,
-                        [System.Management.Automation.CompletionResultType]::ParameterValue,
-                        ('Driver Type: {0}' -f $_ )
-                    )
+if ($PSVersionTable.PSVersion.Major -ge 5) {
+    function Get-OMPlusTypeTable {
+        [cmdletbinding()]
+        param(
+            [parameter(Mandatory)]
+            [ArgumentCompleter({
+                param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+                    Get-OMPlusDriverNames | Select-object -ExpandProperty 'Driver' |
+                    Where-Object { $_ -like "$WordToComplete*"} |
+                    Sort-Object |
+                    Foreach-Object {
+                        [System.Management.Automation.CompletionResult]::new(
+                            $_,
+                            $_,
+                            [System.Management.Automation.CompletionResultType]::ParameterValue,
+                            ('Driver Type: {0}' -f $_ )
+                        )
+                    }
+            })]
+            [string]$DriverType,
+
+            [parameter()]
+            [ValidateSet('Trays','PaperSizes','MediaTypes')]
+            [string]$DisplayType = 'Trays',
+
+            [parameter()]
+            [ValidateSet('Type','ID')]
+            [string]$SortBy = 'Type'
+        )
+
+        $TypesFile = [system.io.path]::Combine($Global:OMHOMEPATH, 'system', 'types.conf')
+        $XML = New-Object -TypeName XML
+        $XML.Load($TypesFile)
+
+        switch ($DisplayType) {
+            'Trays' {
+                switch ($SortBy) {
+                    'Type' { $SortType = 'TrayRef' }
+                    'ID'   { $SortType = 'ID' }
                 }
-        })]
-        [string]$DriverType,
-
-        [parameter()]
-        [ValidateSet('Trays','PaperSizes','MediaTypes')]
-        [string]$DisplayType = 'Trays',
-
-        [parameter()]
-        [ValidateSet('Type','ID')]
-        [string]$SortBy = 'Type'
-    )
-
-    $TypesFile = [system.io.path]::Combine($Global:OMHOMEPATH, 'system', 'types.conf')
-    $XML = New-Object -TypeName XML
-    $XML.Load($TypesFile)
-
-    switch ($DisplayType) {
-        'Trays' {
-            switch ($SortBy) {
-                'Type' { $SortType = 'TrayRef' }
-                'ID'   { $SortType = 'ID' }
+                $Trays = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/TRAYS/TRAY' -f $DriverType)  | Select-Object -ExpandProperty Node
+                $Trays | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='TrayID';e={$_.id}} |
+                    Where-Object { $null -ne $_.TrayRef } | Sort-Object -Property $SortType
             }
-            $Trays = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/TRAYS/TRAY' -f $DriverType)  | Select-Object -ExpandProperty Node
-            $Trays | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='TrayID';e={$_.id}} |
-                Where-Object { $null -ne $_.TrayRef } | Sort-Object -Property $SortType
+            'PaperSizes' {
+                switch ($SortBy) {
+                    'Type' { $SortType = 'PaperSizeRef' }
+                    'ID'   { $SortType = 'ID' }
+                }
+                $Paper = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/PSIZE/PAPER' -f $DriverType) | Select-Object -ExpandProperty Node
+                $Paper | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='PaperSizeID';e={$_.id}} |
+                    Where-Object { $null -ne $_.PaperSizeRef } | Sort-Object -Property $SortType
+            }
+            'MediaTypes' {
+                switch ($SortBy) {
+                    'Type' { $SortType = 'MediaTypeRef' }
+                    'ID'   { $SortType = 'ID' }
+                }
+                $Media = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/MTYPE/MEDIA' -f $DriverType) | Select-Object -ExpandProperty Node
+                $Media | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='MediaTypeID';e={$_.id}} |
+                    Where-Object {$null -ne $_.MediaTypeRef} |Sort-Object -Property $SortType
+            }
         }
-        'PaperSizes' {
-            switch ($SortBy) {
-                'Type' { $SortType = 'PaperSizeRef' }
-                'ID'   { $SortType = 'ID' }
-            }
-            $Paper = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/PSIZE/PAPER' -f $DriverType) | Select-Object -ExpandProperty Node
-            $Paper | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='PaperSizeID';e={$_.id}} |
-                Where-Object { $null -ne $_.PaperSizeRef } | Sort-Object -Property $SortType
+    }
+}
+else {
+    function Get-OMPlusTypeTable {
+        [cmdletbinding()]
+        param(
+            [parameter(Mandatory)]
+            [string]$DriverType,
+
+            [parameter()]
+            [ValidateSet('Trays','PaperSizes','MediaTypes')]
+            [string]$DisplayType = 'Trays',
+
+            [parameter()]
+            [ValidateSet('Type','ID')]
+            [string]$SortBy = 'Type'
+        )
+
+        if ($DriverType -in $Global:ValidTypes) {
+            Write-Verbose -Message 'Valid type, continuing'
         }
-        'MediaTypes' {
-            switch ($SortBy) {
-                'Type' { $SortType = 'MediaTypeRef' }
-                'ID'   { $SortType = 'ID' }
+        else {
+            throw ('DriverType ({0}) does not appear to be a valid driver type' -f $DriverType)
+        }
+
+        $TypesFile = [system.io.path]::Combine($Global:OMHOMEPATH, 'system', 'types.conf')
+        $XML = New-Object -TypeName XML
+        $XML.Load($TypesFile)
+
+        switch ($DisplayType) {
+            'Trays' {
+                switch ($SortBy) {
+                    'Type' { $SortType = 'TrayRef' }
+                    'ID'   { $SortType = 'ID' }
+                }
+                $Trays = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/TRAYS/TRAY' -f $DriverType)  | Select-Object -ExpandProperty Node
+                $Trays | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='TrayID';e={$_.id}} |
+                    Where-Object { $null -ne $_.TrayRef } | Sort-Object -Property $SortType
             }
-            $Media = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/MTYPE/MEDIA' -f $DriverType) | Select-Object -ExpandProperty Node
-            $Media | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='MediaTypeID';e={$_.id}} |
-                Where-Object {$null -ne $_.MediaTypeRef} |Sort-Object -Property $SortType
+            'PaperSizes' {
+                switch ($SortBy) {
+                    'Type' { $SortType = 'PaperSizeRef' }
+                    'ID'   { $SortType = 'ID' }
+                }
+                $Paper = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/PSIZE/PAPER' -f $DriverType) | Select-Object -ExpandProperty Node
+                $Paper | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='PaperSizeID';e={$_.id}} |
+                    Where-Object { $null -ne $_.PaperSizeRef } | Sort-Object -Property $SortType
+            }
+            'MediaTypes' {
+                switch ($SortBy) {
+                    'Type' { $SortType = 'MediaTypeRef' }
+                    'ID'   { $SortType = 'ID' }
+                }
+                $Media = Select-XML -XML $XML -XPath ('//PTYPE[@name="{0}"]/MTYPE/MEDIA' -f $DriverType) | Select-Object -ExpandProperty Node
+                $Media | Select-Object -Property @{n=$SortType;e={($_.'#text').Trim()}},@{n='MediaTypeID';e={$_.id}} |
+                    Where-Object {$null -ne $_.MediaTypeRef} |Sort-Object -Property $SortType
+            }
         }
     }
 }
