72b40d1fe554f5f6260ade12ffb099892ba876a9
DavidFigueroa <david.figueroa@harrishealth.org>
2021-02-05 08:06:47 -0600

    Added #Requires -RunAsAdministrator

--- OMPlus.psm1
+++ OMPlus.psm1
@@ -1,3 +1,5 @@
+#Requires -RunAsAdministrator
+
 Get-ChildItem -Path $PSScriptRoot\Private -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
     ForEach-Object {
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
@@ -8,6 +10,7 @@ Get-ChildItem -Path $PSScriptRoot\Public -File -Filter *.ps1 | Where-Object full
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
         . $_.FullName
     }
+
 
 $Global:OMHOMEPath          = (Get-ItemProperty -Path 'HKLM:\Software\PlusTechnologies\OMPlusServer' -Name OMHOMEPath).OMHOMEPath
 $GLobal:OMPlusPrinterPath   = [System.IO.Path]::Combine($Global:OMHOMEPath, 'printers')
