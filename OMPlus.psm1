f1ace5a32adf53e317d469fba05f634ec58e52df
DavidFigueroa <david.figueroa@harrishealth.org>
2021-02-18 17:36:01 -0600

    Added test for primary MPS

--- OMPlus.psm1
+++ OMPlus.psm1
@@ -1,5 +1,23 @@
 #Requires -RunAsAdministrator
 
+$Global:OMHOMEPath          = (Get-ItemProperty -Path 'HKLM:\Software\PlusTechnologies\OMPlusServer' -Name OMHOMEPath).OMHOMEPath
+$GLobal:OMPlusPrinterPath   = [System.IO.Path]::Combine($Global:OMHOMEPath, 'printers')
+$Global:OMPlusBinPath       = [System.IO.Path]::Combine($Global:OMHOMEPATH, 'bin')
+$Global:OMPlusFormsPath     = [System.IO.Path]::Combine($Global:OMHOMEPATH, 'forms')
+$Global:OMPLusSystemPath    = [System.IO.Path]::Combine($Global:OMHOMEPath, 'system')
+
+$pingMaster = Get-Content -Path ([System.IO.Path]::Combine( $Global:OMPLusSystemPath, 'pingMaster'))
+if ($pingMaster -eq 'none') {
+    $Global:OMPlusPrimaryMPS   = [system.net.dns]::GetHostByName($env:computername).hostname
+    $Global:IsOMPLusPrimaryMPS = $true
+}
+else {
+    $Global:IsOMPLusPrimaryMPS = $false
+    $Global:OMPlusPrimaryMPS   = $pingMaster
+}
+
+Remove-Variable -Name pingMaster
+
 Get-ChildItem -Path $PSScriptRoot\Private -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
     ForEach-Object {
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
@@ -11,12 +29,6 @@ Get-ChildItem -Path $PSScriptRoot\Public -File -Filter *.ps1 | Where-Object full
         . $_.FullName
     }
 
-
-$Global:OMHOMEPath          = (Get-ItemProperty -Path 'HKLM:\Software\PlusTechnologies\OMPlusServer' -Name OMHOMEPath).OMHOMEPath
-$GLobal:OMPlusPrinterPath   = [System.IO.Path]::Combine($Global:OMHOMEPath, 'printers')
-$Global:OMPlusBinPath       = [System.IO.Path]::Combine($Global:OMHOMEPATH, 'bin')
-$Global:OMPlusFormsPath     = [System.IO.Path]::Combine($Global:OMHOMEPATH, 'forms')
-$Global:OMPLusSystemPath    = [System.IO.Path]::Combine($Global:OMHOMEPath, 'system')
 $Global:ValidTypes          = Get-OMPLusDriverNames | Select-Object -ExpandProperty Driver | Sort-Object
 $Global:CRLF                = [Environment]::NewLine
 $Global:OMPlusServerFQDN    = [system.net.dns]::GetHostByName($env:COMPUTERNAME).hostname
