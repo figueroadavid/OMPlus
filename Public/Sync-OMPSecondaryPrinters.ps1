83be0406c219d54696efa70e55048ad1fcb7ee56
DavidFigueroa <david.figueroa@harrishealth.org>
2021-02-18 19:47:04 -0600

    Initial commit for Sync

---
+++ Public/Sync-OMPSecondaryPrinters.ps1
@@ -0,0 +1,91 @@
+functino Sync-OMPSecondaryPrinter {
+    [cmdletbinding()]
+    param(
+        [parameter(ValueFromPipelineByPropertyName)]
+        [string[]]$PrinterName,
+
+        [parameter(ValueFromPipelineByPropertyName)]
+        [switch]$ShowProgress
+    )
+
+    begin {
+        $ProcessStartInfo                           = [System.Diagnostics.ProcessStartInfo]::new()
+        $ProcessStartInfo.FileName                  = [system.io.path]::Combine($Global:OMPlusBinPath, 'dmdestsync.exe')
+        $ProcessStartInfo.WorkingDirectory          = $Global:OMPLusSystemPath
+        $ProcessStartInfo.CreateNoWindow            = $true
+        $ProcessStartInfo.RedirectStandardError     = $true
+        $ProcessStartInfo.RedirectStandardOutput    = $true
+        $ProcessStartInfo.UseShellExecute           = $false
+
+        if ($PrinterName -contains 'All') {
+            Write-Warning -Message 'The PrinterName list contains "All", this will take some time'
+        }
+        $Process = [System.Diagnostics.Process]::new()
+    }
+
+    process {
+        if ($PrinterName -contains 'All') {
+            if ($Global:IsOMPLusPrimaryMPS) {
+                $ProcessStartInfo.Arguments     = '-d all'
+            }
+            else {
+                $ProcessStartInfo.Arguments     = '-r all'
+            }
+            $Process.StartInfo                  = $ProcessStartInfo
+            $Process.Start()
+            $Process.WaitForExit()
+            $Process.StandardError.ReadToEnd()
+            $Process.StandardOutput.ReadToEnd()
+        }
+        else {
+            if ($ShowProgress) {
+                $CurrentCount = 0
+                $ProgressSplat = @{
+                    PercentComplete     = [int][math]::round($CurrentCount/$PrinterName.Count * 100, [System.MidpointRounding]::AwayFromZero)
+                    CurrentOperation    = 'Start'
+                    Status              = 'Starting'
+                }
+                if ($Global:IsOMPLusPrimaryMPS) {
+                    $ProgressSplat['Activity'] = 'Pushing printers from primary MPS to secondary MPS'
+                }
+                else {
+                    $ProgressSplat['Activity'] = 'Pulling printers from primary MPS to secondary MPS'
+                }
+                Write-Progress @ProgressSplat
+            }
+            foreach ($Printer in $PrinterName) {
+                if ($ShowProgress) {
+                    $CurrentCount ++
+                    $ProgressSplat = @{
+                        PercentComplete     = [int][math]::round($CurrentCount/$PrinterName.Count * 100, [System.MidpointRounding]::AwayFromZero)
+                        CurrentOperation    = $Printer
+                        Status              = '{0} of {1}' -f $CurrentCount, $PrinterName.Count
+                    }
+                    if ($Global:IsOMPLusPrimaryMPS) {
+                        $ProgressSplat['Activity'] = 'Pushing printers from primary MPS to secondary MPS'
+                    }
+                    else {
+                        $ProgressSplat['Activity'] = 'Pulling printers from primary MPS to secondary MPS'
+                    }
+                    Write-Progress @ProgressSplat
+                }
+
+                if ($Global:IsOMPLusPrimaryMPS) {
+                    $ProcessStartInfo.Arguments = '-d {0}' -f $Printer
+                }
+                else {
+                    $ProcessStartInfo.Arguments = '-r {0}' -f $Printer
+                }
+                $Process.StartInfo = $ProcessStartInfo
+                $Process.Start()
+                $Process.WaitForExit()
+                $Process.StandardError.ReadToEnd()
+                $Process.StandardOutput.ReadToEnd()
+            }
+        }
+    }
+
+    end {
+        $Process.Close()
+    }
+}
