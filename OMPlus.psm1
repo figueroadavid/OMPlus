4bd979390efc25bcc0839c2df5e154d696b7931d
DavidFigueroa <david.figueroa@harrishealth.org>
2021-02-19 15:55:39 -0600

    Several updates; major changes

--- OMPlus.psm1
+++ OMPlus.psm1
@@ -6,29 +6,37 @@ $Global:OMPlusBinPath       = [System.IO.Path]::Combine($Global:OMHOMEPATH, 'bin
 $Global:OMPlusFormsPath     = [System.IO.Path]::Combine($Global:OMHOMEPATH, 'forms')
 $Global:OMPLusSystemPath    = [System.IO.Path]::Combine($Global:OMHOMEPath, 'system')
 
+Write-Verbose -Message 'Setting parameters for OMPlusPrimaryMPS, OMPlusSecondaryMPS'
 $pingMaster = Get-Content -Path ([System.IO.Path]::Combine( $Global:OMPLusSystemPath, 'pingMaster'))
 if ($pingMaster -eq 'none') {
     $Global:OMPlusPrimaryMPS   = [system.net.dns]::GetHostByName($env:computername).hostname
     $Global:IsOMPLusPrimaryMPS = $true
+
+    $pingParmPath = [system.io.path]::combine($Global:OMPLusSystemPath, 'pingParms')
+    $Global:OMPlusSecondaryMPS = (Get-Content -Path $pingParmPath |
+        Where-Object { $_ -match '^Backup'}).Split('=')[1]
 }
 else {
     $Global:IsOMPLusPrimaryMPS = $false
     $Global:OMPlusPrimaryMPS   = $pingMaster
+    $Global:OMPlusSecondaryMPS = [System.Net.Dns]::GetHostByName($env:computername).hostname
 }
 
-Remove-Variable -Name pingMaster
+Remove-Variable -Name pingMaster,pingParmPath
 
 Get-ChildItem -Path $PSScriptRoot\Private -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
     ForEach-Object {
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
-        . $_.FullName }
+        . $_.FullName
+    }
 
 Get-ChildItem -Path $PSScriptRoot\Public -File -Filter *.ps1 | Where-Object fullname -notmatch '`.tests`.ps1' |
     ForEach-Object {
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
         . $_.FullName
     }
+if ($Global:IsOMPLusPrimaryMPS) {
+    $Global:ValidTypes      = Get-OMPLusDriverNames | Select-Object -ExpandProperty Driver | Sort-Object
+}
 
-$Global:ValidTypes          = Get-OMPLusDriverNames | Select-Object -ExpandProperty Driver | Sort-Object
 $Global:CRLF                = [Environment]::NewLine
-$Global:OMPlusServerFQDN    = [system.net.dns]::GetHostByName($env:COMPUTERNAME).hostname
