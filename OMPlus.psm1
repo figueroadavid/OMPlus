51e8bf50297b61e328d776d3e9b76a160a476454
DavidFigueroa <david.figueroa@harrishealth.org>
2021-05-26 17:05:05 -0500

    Updated variables to $script: variables

--- OMPlus.psm1
+++ OMPlus.psm1
@@ -1,25 +1,25 @@
 #Requires -RunAsAdministrator
 
-$OMPrinterPath   = [System.IO.Path]::Combine($env:OMHOME, 'printers')
-$OMBinPath       = [System.IO.Path]::Combine($env:OMHOME, 'bin')
-$OMFormsPath     = [System.IO.Path]::Combine($env:OMHOME, 'forms')
-$OMSystemPath    = [System.IO.Path]::Combine($env:OMHOME, 'system')
-$OMModelPath     = [System.IO.Path]::Combine($env:OMHOME, 'model')
+$Script:OMPrinterPath   = [System.IO.Path]::Combine($env:OMHOME, 'printers')
+$Script:OMBinPath       = [System.IO.Path]::Combine($env:OMHOME, 'bin')
+$Script:OMFormsPath     = [System.IO.Path]::Combine($env:OMHOME, 'forms')
+$Script:OMSystemPath    = [System.IO.Path]::Combine($env:OMHOME, 'system')
+$Script:OMModelPath     = [System.IO.Path]::Combine($env:OMHOME, 'model')
 
 Write-Verbose -Message 'Setting parameters for OMPrimaryMPS, OMSecondaryMPS'
 $pingMaster = Get-Content -Path ([System.IO.Path]::Combine( $OMSystemPath, 'pingMaster'))
 if ($pingMaster -eq 'none') {
-    $OMPrimaryMPS   = [system.net.dns]::GetHostByName($env:computername).hostname
-    $IsOMPrimaryMPS = $true
+    $Script:OMPrimaryMPS   = [system.net.dns]::GetHostByName($env:computername).hostname
+    $Script:IsOMPrimaryMPS = $true
 
     $pingParmPath   = [system.io.path]::combine($OMSystemPath, 'pingParms')
-    $OMSecondaryMPS = (Get-Content -Path $pingParmPath |
+    $Script:OMSecondaryMPS = (Get-Content -Path $pingParmPath |
         Where-Object { $_ -match '^Backup'}).Split('=')[1]
 }
 else {
-    $IsOMPrimaryMPS = $false
-    $OMPrimaryMPS   = $pingMaster
-    $OMSecondaryMPS = [System.Net.Dns]::GetHostByName($env:computername).hostname
+    $Script:IsOMPrimaryMPS = $false
+    $Script:OMPrimaryMPS   = $pingMaster
+    $Script:OMSecondaryMPS = [System.Net.Dns]::GetHostByName($env:computername).hostname
 }
 
 Remove-Variable -Name pingMaster,pingParmPath -ErrorAction SilentlyContinue
@@ -35,9 +35,11 @@ Get-ChildItem -Path $PSScriptRoot\Public -File -Filter *.ps1 | Where-Object full
         Write-Verbose -Message ('Importing script: {0}' -f $_.FullName ) -Verbose
         . $_.FullName
     }
+
 if ($IsOMPrimaryMPS) {
-    $ValidTypes      = Get-OMDriverNames | Select-Object -ExpandProperty Driver | Sort-Object
+    $script:ValidTypes      = Get-OMDriverNames | Select-Object -ExpandProperty Driver | Sort-Object
 }
-$ValidModels = Get-ChildItem -Path $OMModelPath | Select-Object -ExpandProperty Name 
+
+$script:ValidModels = Get-ChildItem -Path $OMModelPath | Select-Object -ExpandProperty Name
 
 $CRLF                = [Environment]::NewLine
